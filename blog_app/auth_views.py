f8c6416e1df9f34384d4ee80df9be6932d3d6f11
Priscilla Baah <pyfbaah@gmail.com>
2022-11-03 00:47:03 +0000

    get form data

--- blog_app/auth_views.py
+++ blog_app/auth_views.py
@@ -1,13 +1,22 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 
 auth_views = Blueprint("auth_views", __name__)
 
-@auth_views.route('/sign-up')
+@auth_views.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+    
     return render_template('sign_up.html')
 
-@auth_views.route('/login')
+@auth_views.route('/login', methods=['GET', 'POST'])
 def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
     return render_template('login.html')
 
 @auth_views.route('/logout')

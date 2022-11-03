77ce6d01d01bcea935d7760bfd8ec171cd9d92f1
Priscilla Baah <pyfbaah@gmail.com>
2022-11-03 06:12:22 +0000

    remove unneeded comments

--- app.py
+++ app.py
@@ -3,17 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from blog_app import create_app
 
 app = create_app()
-# db = SQLAlchemy(app)
-
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     email = db.Column()
-
-
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)

ad7d7a74981d61b1436e107ad415a836dae1a540
Priscilla Baah <pyfbaah@gmail.com>
2022-11-04 19:12:01 +0000

    make fields non-null, add post model

--- blog_app/models.py
+++ blog_app/models.py
@@ -6,9 +6,19 @@ db = SQLAlchemy()
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(150))
-    last_name = db.Column(db.String(150))
-    username = db.Column(db.String(150), unique=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150), nullable=False)
+    last_name = db.Column(db.String(150), nullable=False)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    # posts = db.relationship('Post', backref='user', passive_deletes=True)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(500), nullable=False)
+    body = db.Column(db.Text(), nullable=False)
+    category= db.Column(db.String(150), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey(User.id, ondelete='CASCADE'), nullable=False)
+    author = db.orm.relationship(User)

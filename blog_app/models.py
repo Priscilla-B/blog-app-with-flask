b9fcb581879d625510a24f297261793f333cdc3d
Priscilla Baah <pyfbaah@gmail.com>
2022-11-04 09:35:33 +0000

    create user model

---
+++ blog_app/models.py
@@ -0,0 +1,14 @@
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
+    username = db.Column(db.String(150), unique=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    date_created = db.Column(db.DateTime(timezone=True), default=func.now())

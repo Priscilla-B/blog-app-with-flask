5137d5f4bdb3cf996d3a89faf3346399110e1d09
Priscilla Baah <pyfbaah@gmail.com>
2022-11-05 17:18:00 +0000

    create post form logic

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import current_user, login_required
+
+from .forms import CreatePostForm
+from .models import Post, Tag, User, db
 
 views = Blueprint("views", __name__)
 
@@ -16,12 +20,48 @@ def contact():
     return render_template('contact.html')
 
 @views.route('/create_post', methods=['GET', 'POST'])
+@login_required
 def create_post():
 
+    form = CreatePostForm(request.form)
+    user_id = current_user.get_id()
     if request.method == 'POST':
-        title = request.form.get('title')
-        body = request.form.get('body')
-        category = request.form.get('category')
+       
+        new_post = Post(
+            title = form.title.data,
+            body = form.body.data,
+            author_id = user_id,
+            author = User.query.get(user_id)
+        )
+        
+        for tag in form.tags.data:
+            tag_obj = Tag.query.filter_by(name=tag).first()
+            tag_obj.posts_associated.append(new_post)
+            db.session.add(tag_obj)
+            
+
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect(url_for('views.home'))
+
+    return render_template('create_post.html', form=form)
 
 
-    return render_template('create_post.html')
+@views.route('create_post/add_tag', methods=['GET', 'POST'])
+@login_required
+def add_tag():
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        tag_exists = Tag.query.filter_by(name=name).first()
+        if tag_exists:
+            flash('Tag already exists! Check select input to choose')
+        else:
+            new_tag = Tag(name=name)
+            db.session.add(new_tag)
+            db.session.commit()
+
+        return redirect(url_for('views.create_post'))
+
+    return render_template('add_tag.html')

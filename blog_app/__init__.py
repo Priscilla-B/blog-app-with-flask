dcc238759b4a94e86464eadbf81665c0f31c2a7b
Priscilla Baah <pyfbaah@gmail.com>
2022-11-06 23:24:18 +0000

    add mail settings

--- blog_app/__init__.py
+++ blog_app/__init__.py
@@ -1,20 +1,31 @@
-from os import path
+import os
 
-from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 from flask_login import LoginManager
 
 from .views import views
 from .auth_views import auth_views
 from .models import User, db, migrate
+from .views import mail
+
+from dotenv import load_dotenv
+load_dotenv('.env') 
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'somerandomletters'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+    app.config['MAIL_SERVER']='smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USERNAME'] = os.environ['MAIL_USERNAME']
+    app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
     db.init_app(app)
     migrate.init_app(app, db)
+
+    mail.init_app(app)
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth_views, url_prefix='/')
